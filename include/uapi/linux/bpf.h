68c6abfac7a822700a29b8133ae7a5355cbf8499
Sargun Dhillon <sargun@sargun.me>
2021-03-30 04:12:05 -0500

    bpf, seccomp: Add eBPF filter capabilities
    This introduces the BPF_PROG_TYPE_SECCOMP bpf program type. It is meant
    to be used for seccomp filters as an alternative to cBPF filters. The
    program type has relatively limited capabilities in terms of helpers,
    but that can be extended later on.
    
    The eBPF code loading is separated from attachment of the filter, so
    a privileged user can load the filter, and pass it back to an
    unprivileged user who can attach it and use it at a later time.
    
    In order to attach the filter itself, you need to supply a flag to the
    seccomp syscall indicating that a eBPF filter is being attached, as
    opposed to a cBPF one. Verification occurs at program load time,
    so the user should only receive errors related to attachment.
    
    The behavior of eBPF filters with bitmap cache is that they will
    totally negate the bitmap acceleration. Static analysis of eBPF to
    create a bitmap could be a potential future work. [YiFei Zhu]
    
    All standard BPF helper calls are supported. If the loader has
    CAP_BPF and CAP_PERFMON, all tracing BPF helper calls are also
    supported. The reason for this is that, this adds no new attack
    vectors I (YiFei) can tell. Standard helpers are already accessible
    via BPF_PROG_TYPE_SOCKET_FILTER from an unprivileged loader. Any
    policies that can be set through seccomp-eBPF can already be set
    via seccomp user notifier, if the user uses this feature to implement
    a policy. If a user okay with such advanced features, they may
    implement an LSM policy to restrict this. This LSM hook is added in
    a later patch. [YiFei Zhu]
    
    rcu_read_lock is also held because maps like BPF_MAP_TYPE_LRU_HASH
    requires them. For simplicity, this also affects cBPF filters.
    [YiFei Zhu]
    
    Signed-off-by: Sargun Dhillon <sargun@sargun.me>
    Link: https://lists.linux-foundation.org/pipermail/containers/2018-February/038572.html
    Co-developed-by: Jinghao Jia <jinghao7@illinois.edu>
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>
    Co-developed-by: YiFei Zhu <yifeifz2@illinois.edu>
    Signed-off-by: YiFei Zhu <yifeifz2@illinois.edu>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -949,6 +949,7 @@ enum bpf_prog_type {
 	BPF_PROG_TYPE_LSM,
 	BPF_PROG_TYPE_SK_LOOKUP,
 	BPF_PROG_TYPE_SYSCALL, /* a program that can execute syscalls */
+	BPF_PROG_TYPE_SECCOMP,
 };
 
 enum bpf_attach_type {
