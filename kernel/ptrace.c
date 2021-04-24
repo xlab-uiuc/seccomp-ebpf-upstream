c7223eb0dec969ce97f195249f3d2284d5c77af2
Sargun Dhillon <sargun@sargun.me>
2021-04-24 09:31:58 -0500

    seccomp, ptrace: Add a mechanism to retrieve attached eBPF seccomp filters
    This extends the ptrace API to allow fetching eBPF seccomp filters
    attached to programs. This is to enable checkpoint / restore cases.
    The user will have to use the traditional PTRACE_SECCOMP_GET_FILTER
    API call, and if they get an invalid medium type error they can switch
    over to the eBPF variant of the API -- PTRACE_SECCOMP_GET_FILTER_EXTENDED.
    
    Signed-off-by: Sargun Dhillon <sargun@sargun.me>
    Link: https://lists.linux-foundation.org/pipermail/containers/2018-February/038478.html
    [YiFei: increase ptrace number to 0x4210]
    Signed-off-by: YiFei Zhu <yifeifz2@illinois.edu>

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -1263,6 +1263,10 @@ int ptrace_request(struct task_struct *child, long request,
 		break;
 #endif
 
+	case PTRACE_SECCOMP_GET_FILTER_EXTENDED:
+		ret = seccomp_get_filter_extended(child, addr, datavp);
+		break;
+
 	default:
 		break;
 	}
